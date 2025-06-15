bdeffee14fb7fa3c6f197e846accb37fc029f0b0
Manasi Kale <miss.manasikale@gmail.com>
2025-06-15 20:39:19 +0530

    fix: update token retrieval in auth middleware to use correct cookie name

--- API/middleware/auth.js
+++ API/middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
- const token = req.cookies.access_token; 
+ const token = req.cookies.token; 
  
   if (!token) return res.status(403).json({ message: 'Token required' });
 
